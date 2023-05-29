35ade9d0f2c9cf382ca7d85cf8d6acca91bf9b70
Vikram Rao <contact@vikramrao.in>
2023-05-29 10:43:35 +0530

    Made all tests pass to avoid red markers in demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,6 @@ router.get('/findAvailableProducts', function (req, res, next) {
     axios
         .get('http://localhost:9000/products?type=' + req.query.type)
         .then(apiRes => {
-            console.log(apiRes.data);
             res.send(apiRes.data);
         })
         .catch(err => console.log('Error: ', err.message));
--- test/unit/string.utils.test.js
+++ test/unit/string.utils.test.js
@@ -1,2 +1,2 @@
-test('Uppercase string', () => expect('Specmatic'.toUpperCase()).toBe('specmatic'));
+test('Uppercase string', () => expect('Specmatic'.toUpperCase()).toBe('SPECMATIC'));
 test('Lowercase string', () => expect('Specmatic'.toLowerCase()).toBe('specmatic'));
