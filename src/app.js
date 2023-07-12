09a952965143be65def3a696203a1fc548901350
Vikram Rao <contact@vikramrao.in>
2023-07-12 22:02:35 +0530

    Prevent express logging of requests to keep test output clean

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const logger = require('morgan')
 const specmaticUtil = require('specmatic-util')
 
 const productsRouter = require('./routes/product')
 
 const app = express()
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
