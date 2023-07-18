81e9111d3bf95180cdf57bd14fbd5f89bce3e6a5
Vikram Rao <contact@vikramrao.in>
2023-07-18 22:40:08 +0530

    Windows in github CI needs a long timeout as `java -jar` exec seems to be slow at times

--- test/api/product.test.js
+++ test/api/product.test.js
@@ -1,3 +1,5 @@
+const TEST_TIMEOUT_MS = 10 * 60 * 1000; //10 mins (Windows in github CI needs a long timeout
+                                        //  as java -jar exec seems to be slow at times)
 const KAFKA_BROKER_PORT = 10001
 
 //This has to be at the top as require('../../src/app.js) will expect this on require itself
@@ -15,7 +17,7 @@ beforeAll(async () => {
     httpStub = await specmatic.startHttpStub()
     await specmatic.setHttpStubExpectations('test-resources/products.json', httpStub.url)
     await specmatic.setKafkaMockExpectations(kafkaMock, [{ topic: 'product-queries', count: 1 }])
-}, 50000)
+}, TEST_TIMEOUT_MS)
 
 test('findAvailableProducts gives a list of products', async () => {
     const res = await request(app).get('/findAvailableProducts?type=gadget').accept('application/json').expect(200)
@@ -25,12 +27,12 @@ test('findAvailableProducts gives a list of products', async () => {
     const { type, ...message } = stubBody[0]
     await expect(specmatic.verifyKafkaStubMessage(kafkaMock, 'product-queries', JSON.stringify(message))).resolves.toBeTruthy()
     await expect(specmatic.verifyKafkaStub(kafkaMock)).resolves.toBeTruthy()
-}, 50000)
+}, TEST_TIMEOUT_MS)
 
 afterAll(async () => {
     await specmatic.stopStub(httpStub)
     await specmatic.stopKafkaStub(kafkaMock)
-}, 50000)
+}, TEST_TIMEOUT_MS)
 
 function readJsonFile(stubDataFile) {
     const fs = require('fs')
